edc3c709cccc7733bb543bad474e0afb1c3cefb8
Dan Nelson <danielbnelson@gmail.com>
2021-03-12 19:35:24 +0100

    fix typo

--- 0-file_paths.R
+++ 0-file_paths.R
@@ -4,7 +4,7 @@
 #Script: 1-data_to_rts.R
 
 #This directory contains the subdirectories "CRU" "EOBS" "NCEP" and "TELE" 
-#Each of these subdirectores contains original data files as downloaded from source
+#Each of these subdirectories contains original data files as downloaded from source
 pred_src_data_path<-"INSERT FILE PATH HERE" 
 
 #This directory is the location where cropped data will be placed after transformation to raster time series (rts) objects
@@ -14,7 +14,7 @@ pred_data_out_path<-"INSERT FILE PATH HERE"
 
 #Script: 3-read in points and extract spatial data.R
 
-#Destination directory for writing exported csv containg all observations for complete training and validation data sets
+#Destination directory for writing exported csv containing all observations for complete training and validation data sets
 output.directory.path<-"INSERT FILE PATH HERE"
 #filename for csv
 output.filename.path<- "INSERT FILE PATH HERE"
